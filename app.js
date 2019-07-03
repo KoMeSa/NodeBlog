09a262e82c2c6071e896091e6cecd09ccfdc786a
Ruslan Komesa <komesarus777@gmail.com>
2019-07-03 13:44:31 +0300

    The first page is being tested

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const home = require('./router/home.js');
 
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
-
+app.use (express.static(path.join(__dirname, "public")));
 
 
 app.use(home);
