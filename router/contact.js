56975b4392719f7507ef5461cae2b163210c9d51
Ruslan Komesa <komesarus777@gmail.com>
2019-07-10 15:38:51 +0300

    add form with registered new user (test form)

--- router/contact.js
+++ router/contact.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const url = bodyParser.urlencoded({extended: false});
+const User = require('../models/users.js');
+
+
+
+// const bill = new User({
+//     name: 'bill',
+//     password: '2222'
+// })
+
+// bill.save().then(()=>console.log("yeap"));
+
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+
 
 
 
@@ -9,6 +28,27 @@ router.get('/contact', (req, res)=>{
         
     });
 });
+
+router.post('/contact', url, (req, res)=>{
+    let name = req.body.name;
+    let password = req.body.password;
+    console.log(`name: ${name}, password: ${password}`);
+    
+ const user = new User({
+    name:  name,
+    password: password
+ })
+
+ user.save()
+    .then((user)=>{
+        res.render('contact', { 
+            answer: 'You are registered'
+        });
+ })
+})
+
+
+
 
 
 module.exports = router;
